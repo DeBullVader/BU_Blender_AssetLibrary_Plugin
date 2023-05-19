220d3ae9f84767d2b07fde676d7fa29081c75020
DeBullVader <debullvader@gmail.com>
2023-05-19 17:03:56 +0200

    removed unneeded code

---
+++ utils/__init__.py
@@ -0,0 +1,9 @@
+import bpy
+from .catfile_handler import CatalogsHelper  #dont need to register as its a class that does not inherit from blender API
+
+
+def register():
+   pass
+
+def unregister():
+    pass
