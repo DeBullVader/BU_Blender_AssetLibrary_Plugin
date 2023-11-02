0f1106f2fd5a7e48d313e50bfcf91ab5796eaa9a
DeBullVader <debullvader@gmail.com>
2023-11-02 19:12:22 +0100

    Added Remove asset / and test button

--- operators/download_library_files.py
+++ operators/download_library_files.py
@@ -2,6 +2,8 @@
 # from __future__ import print_function
 import bpy
 import logging
+import os
+from bpy.types import Context
 from .file_managment import AssetSync
 from ..utils import addon_info,exceptions,progress
 from . import task_manager
@@ -27,13 +29,17 @@ class BU_OT_Download_Original_Library_Asset(bpy.types.Operator):
         if payed == False and current_library_name ==addon_info.get_lib_name(True,addon_prefs.debug_mode):
             cls.poll_message_set('Please input a valid BUK premium license key')
             return False
-        elif len(selected_assets)>10:
+        if len(selected_assets)>10:
             cls.poll_message_set('Only 10 assets are allowed to can be downloaded at once.')
             return False
-        elif BU_OT_Download_Original_Library_Asset.asset_sync_instance or task_manager.task_manager_instance:
+        if BU_OT_Download_Original_Library_Asset.asset_sync_instance or task_manager.task_manager_instance:
+            cls.poll_message_set('an instance is already running. Please wait or cancel it.')
             return False
         if addon_prefs.lib_path =='':
             cls.poll_message_set('Please input a valid library path. Go to Asset Browser settings in the BU side panel')
+            return False
+        if not context.selected_asset_files:
+            cls.poll_message_set('Please select assets to download')
             return False
         return True
     
@@ -45,6 +51,7 @@ class BU_OT_Download_Original_Library_Asset(bpy.types.Operator):
                 isPremium = True
             else:
                 isPremium = False
+                
             if BU_OT_Download_Original_Library_Asset.asset_sync_instance:
                 BU_OT_Download_Original_Library_Asset.asset_sync_instance.sync_original_assets(context,isPremium)
 
@@ -83,31 +90,101 @@ class BU_OT_Download_Original_Library_Asset(bpy.types.Operator):
             bpy.ops.wm.initialize_task_manager()
             BU_OT_Download_Original_Library_Asset.asset_sync_instance = AssetSync()
             BU_OT_Download_Original_Library_Asset.asset_sync_instance.current_state = 'fetch_original_asset_ids'
+            # BU_OT_Download_Original_Library_Asset.asset_sync_instance.current_state = 'sync_original_assets'
         except Exception as e:
             print(f"An error occurred: {e}")
+            bpy.ops.bu.c
         return{'RUNNING_MODAL'}
     
     def cancel(self, context):
         wm = context.window_manager
         wm.event_timer_remove(self._timer)
      
+
+
+class BU_OT_Remove_Library_Asset(bpy.types.Operator):
+    bl_idname = "bu.remove_library_asset"
+    bl_label = "Remove library asset"
+    bl_options = {"REGISTER"}
+
+    @classmethod
+    def poll(cls, context):
+        if not context.selected_asset_files:
+            return False
+        return True
+        
+    def execute(self, context):
+        addonprefs = addon_info.get_addon_name().preferences
+        current_library_name = context.area.spaces.active.params.asset_library_ref
+       
+        for asset in context.selected_asset_files:
+            asset_path = f'{addonprefs.lib_path}{os.sep}{current_library_name}{os.sep}{asset.name}{os.sep}{asset.name}.blend'
+            ph_path = f'{addonprefs.lib_path}{os.sep}{current_library_name}{os.sep}{asset.name}{os.sep}PH_{asset.name}.blend'
+            if os.path.exists(asset_path):
+                os.remove(asset_path)
+                bpy.ops.asset.library_refresh()
+            
+            elif os.path.exists(ph_path):
+                os.remove(ph_path)
+                bpy.ops.asset.library_refresh()
+
+        return {'FINISHED'}
+    
+class BU_OT_AppendToScene(bpy.types.Operator):
+    bl_idname = "bu.append_to_scene"
+    bl_label = "Append to scene"
+    bl_options = {"REGISTER"}
+
+    def execute(self, context):
+        target_lib = addon_info.get_target_lib().path
+        baseName = "Coffeeplant_Small.001"
+        ph_file = f'{target_lib}{os.sep}{baseName}{os.sep}PH_{baseName}.blend'
+        print(ph_file)
+        # if os.path.exists(ph_file):
+        #     os.rename(ph_file, ph_file.removesuffix('.blend'))
+               
+        if baseName in bpy.context.blend_data.objects:
+            obj = bpy.context.blend_data.objects.get(baseName)
+            
+            bpy.context.scene.collection.objects.link(obj)
+            # local.asset_mark()
+            bpy.ops.asset.library_refresh()
+            # if obj.name == "Coffeeplant_Small.001":
+            #     # local = obj.make_local()
+            #     target_lib = addon_info.get_target_lib().path
+                
+            #     ph_file = f'{target_lib}{os.sep}{obj.name}{os.sep}PH_{obj.name}.blend'
+            #     print(ph_file)
+            #     if os.path.exists(ph_file):
+
+            #         os.rename(ph_file, ph_file.removesuffix('.blend'))
+            #         bpy.ops.asset.library_refresh()
+                        
+                
+                # bpy.context.scene.collection.objects.link(obj)
+                # print(local.__dir__())
+                # print(obj.__dir__()) 
+        return {'FINISHED'} 
+     
 def draw_download_asset(self, context):
     layout = self.layout
     layout.operator(BU_OT_Download_Original_Library_Asset.bl_idname, text='Download original asset', icon='URL')
-
-
+    layout.operator("bu.remove_library_asset", text='Remove library asset', icon='URL')
+    layout.operator("bu.append_to_scene", text='Append to scene', icon='URL')
 
 classes =(
     BU_OT_Download_Original_Library_Asset,
+    BU_OT_Remove_Library_Asset,
+    BU_OT_AppendToScene
 )
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
-        bpy.types.ASSETBROWSER_MT_context_menu.prepend(draw_download_asset)
+    bpy.types.ASSETBROWSER_MT_context_menu.prepend(draw_download_asset)
         
 
 def unregister():
     for cls in classes:
         bpy.utils.unregister_class(cls)
-        bpy.types.ASSETBROWSER_MT_context_menu.remove(draw_download_asset)
+    bpy.types.ASSETBROWSER_MT_context_menu.remove(draw_download_asset)
         
