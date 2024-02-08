11d39e0b8514ea045100d736fe9c550e84ff1be8
DeBullVader <debullvader@gmail.com>
2024-02-08 16:08:04 +0100

    Added reload for admin_tool Needs revision later

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,20 +1,29 @@
 import bpy
 import os
+from importlib import reload
 from . import config
 from . import addon_info
 from . import exceptions
 from . import addon_logger
 from . import drag_drop_handler
-def import_admin_tools():
+def try_import_admin_tool():
    try:
       from . import admin_tool
       return admin_tool
-   except:
-
+   except Exception as e:
       return None
    
-admin_tool = import_admin_tools()
-
+admin_tool=try_import_admin_tool()
+if bpy in locals():
+   if "admin_tool" in locals():
+      print('admin tool found')
+      admin_tool = reload(admin_tool)
+   else:
+      try:
+         from . import admin_tool
+      except:
+         print('admin tool not found')
+         pass
 
 def register():
    addon_info.register()
