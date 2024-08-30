901600daab91ebe27134fee4147e06c1faa9eee0
DeBullVader <debullvader@gmail.com>
2024-08-30 17:26:44 +0200

    added bl_idname = __package__ to config_props

--- utils/config.py
+++ utils/config.py
@@ -10,6 +10,7 @@ from .constants import (
 
 
 class config_props(bpy.types.AddonPreferences):
+    bl_idname = __package__
     addon_prefs = addon_info.get_addon_name().preferences
 
     upload_folder_id: bpy.props.StringProperty(
